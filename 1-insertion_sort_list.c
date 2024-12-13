9bf5e964ef8277d46a3f935da8488b0752f1e52f
ilmi-p <iveliu96@gmail.com>
2024-12-13 09:50:27 +0000

    Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,46 +1,44 @@
 #include "sort.h"
-#include <string.h>
-
-
-void switching(listint_t *valeur, listint_t *previous)
-{
-	listint_t *temp2 = NULL;
-
-	temp2 = previous;
-	previous = valeur;
-	valeur = temp2;
-
-}
-
 /**
- * insertion_sort_list - sort doubly linked list by insertion sort
- * @list: double pointer to doubly linked list
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending
+ * order by rearranging nodes in place.
  *
- * Return:void
+ * @list: Pointer to the head of the list.
  */
-
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current = NULL;
-	listint_t *temp = NULL;
+	listint_t *current;
+	listint_t *temp;
+	listint_t *prev;
 
-	current = *(list);
-
-	if (list == NULL)
-	{
+	if (!list || !*list || !(*list)->next)
 		return;
-	}
 
-	while (current->next != NULL)
+	current = (*list)->next;
+	while (current)
 	{
-		if (current->n > current->next->n)
-		{
-			printf("current next si plus petit %d\n", current->next->n);
-			temp = current;
-			switching(temp, current->prev);
-		}
-		current = current->next;
-	}
+	prev = current->prev;
+	while (prev && current->n < prev->n)
+	{
+	temp = current->next;
+	if (prev->prev)
+	prev->prev->next = current;
+	current->prev = prev->prev;
+
+	prev->next = temp;
+	if (temp)
+	temp->prev = prev;
+
+	current->next = prev;
+	prev->prev = current;
+
+	if (!current->prev)
+	*list = current;
+
 	print_list(*list);
 
+	prev = current->prev;
+	}
+current = current->next;
+	}
 }
