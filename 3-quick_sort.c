d1e170872d778bb55c5955d832bea29f79bc1f86
korneel <korneel.loy@gmail.com>
2024-12-13 11:13:29 +0100

    quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,69 @@
+#include "sort.h"
+
+/**
+ * swap - swap 2 elements of a list
+ * @array: pointer to the array
+ * @index1: 1st index to change
+ * @index2: 1st index to change
+ *
+ * Return: void
+ */
+
+void swap(int *array, int index1, int index2)
+{
+	int temp = array[index1];
+	array[index1] = array[index2];
+	array[index2] = temp;
+}
+
+
+/**
+ * quick_sort - sort array accoring to quick sort method
+ * @array: pointer to the array
+ * @size: size of the array
+ *
+ * Return: void
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	int pivot = size - 1, j = 0, i = 0, k, n_one, n_two;
+	int *array_one, *array_two;
+	int temp;
+
+	if (size == 0)
+	{
+		return;
+	}
+
+
+	for (j = 0; j < pivot; j++)
+	{
+		if (array[j] <= array[pivot])
+		{
+			swap(array, i, j);
+			i++;
+		}
+	}
+	
+	temp = array[pivot];
+
+	swap(array, pivot, i);
+	 
+	n_one = i;
+	n_two = pivot - i;
+
+		array_one = malloc(sizeof(int)* n_one);
+		array_two = malloc(sizeof(int)* n_two);
+
+	for (k = 0; k < n_one; k++)
+		array_one[k] = array[k];
+	for (k = i+1; k <= pivot; k++)
+		array_two[k - i - 1] = array[k];
+
+	quick_sort(array_one, n_one);
+	printf("%d\n", temp);
+
+
+	quick_sort(array_two, n_two);
+}
