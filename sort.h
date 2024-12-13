11ce1b5b387d39b4c41fa4b3a78db15862f32665
ilmi-p <iveliu96@gmail.com>
2024-12-13 09:56:00 +0000

    sup of 2 last funtion

--- sort.h
+++ sort.h
@@ -24,6 +24,4 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void sort_node(listint_t **list, listint_t *node);
-void swap_nodes(listint_t **list, listint_t *node);
 #endif
